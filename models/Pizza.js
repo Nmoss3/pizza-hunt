e1aa4dc209cbb90b206e2ea29fee00de43cc03c9
Nmoss3 <75024930+Nmoss3@users.noreply.github.com>
2021-06-01 18:10:34 -0700

    refactored to show timestamp properly 18.2.7

--- models/Pizza.js
+++ models/Pizza.js
@@ -12,6 +12,7 @@ const PizzaSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
       type: String,
@@ -26,9 +27,11 @@ const PizzaSchema = new Schema(
     ],
   },
   {
-    toJSon: {
+    toJSON: {
       virtuals: true,
+      getters: true,
     },
+    // prevents virtuals from creating duplicate of _id as `id`
     id: false,
   }
 );
@@ -38,8 +41,6 @@ PizzaSchema.virtual("commentCount").get(function () {
   return this.comments.length;
 });
 
-// create the Pizza Model using the PizzaSchema
 const Pizza = model("Pizza", PizzaSchema);
 
-// export the Pizza model
 module.exports = Pizza;
